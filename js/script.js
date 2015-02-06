d9ef103b20299c6cad590840b5500fb8b6a4392f
Johannes Böhm <polyhickup@gmail.com>
2015-02-06 21:21:28 +0100

    ..

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 // DODO -- Knockout MVVM integrieren
 
+var myLocations = ko.observableArray();
+myLocations.push( new google.maps.LatLng(47.391404, 8.515527) );
+myLocations.push( new google.maps.LatLng(47.377805, 8.516814) );
+myLocations.push( new google.maps.LatLng(47.382579, 8.505524) );
+myLocations.push( new google.maps.LatLng(47.389494, 8.526681) );
+
+
 var mapOptions = {
-        center: new google.maps.LatLng(40.435833800555567, -78.44189453125),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 11
-      };     
+    center: new google.maps.LatLng(47.391404, 8.515527),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 14
+};     
 
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
@@ -13,20 +20,17 @@ var input = document.getElementById('searchTextField');
 var autocomplete = new google.maps.places.Autocomplete(input);
 
 
-
 var initialize = function(){
 
-}
-
-
-//Event Listeners
-google.maps.event.addListener(marker, 'click', function() {
-    map.setZoom(8);
-    map.setCenter(marker.getPosition());
-  });
+	for(var i=0; i<myLocations().length; i++){
+	  var marker = new google.maps.Marker({
+    	  position: myLocations()[i],
+    	  map: map,
+    	  title: 'Hello World!'
+  		});
+	}
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-
-console.log("test");
+console.log(myLocations().length);
