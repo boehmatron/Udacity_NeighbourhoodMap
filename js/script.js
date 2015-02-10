efbe258b3c2c327fe71692ffbc82967f67b8c3ae
Johannes Böhm <polyhickup@gmail.com>
2015-02-10 21:28:59 +0100

    .

--- js/script.js
+++ js/script.js
@@ -18,25 +18,49 @@
 - Knockout MVVM integrieren
 */
 
+// MODEL
+//----------------
+var myLocations = ko.observableArray([
+  {
+    name: "Name1",
+    street: "Hardturmstrasse 3",
+    city: "Zürich",
+    latlng: new google.maps.LatLng(47.391404, 8.515527) },
+  { 
+    name: "Name2",
+    street: "Bahnhofstr. 10",
+    city: "Zürich", 
+    latlng: new google.maps.LatLng(47.377805, 8.516814) },
+  { 
+    name: "Name3",
+    street: "Badenerstrasse 234",
+    city: "Zürich", 
+    latlng: new google.maps.LatLng(47.382579, 8.505524) }
+]);
+
+ko.applyBindings(myLocations);
+
+
+
+// VIEW
+//----------------
 var mapOptions = {
-    center: new google.maps.LatLng(47.391404, 8.515527),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    zoom: 14
+  center: new google.maps.LatLng(47.391404, 8.515527),
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+  zoom: 14
 };     
 
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 var input = document.getElementById('searchTextField');
 var autocomplete = new google.maps.places.Autocomplete(input);
 
-var myLocations = ko.observableArray([
-    { name: "Name1", latlng: new google.maps.LatLng(47.391404, 8.515527) },
-    { name: "Name2", latlng: new google.maps.LatLng(47.377805, 8.516814) },
-    { name: "Name3", latlng: new google.maps.LatLng(47.382579, 8.505524) }
-]);
+// VIEW MODEL
+//----------------
 
-ko.applyBindings(myLocations);
 
 var initialize = function(){
+
+  var currentMarker = ko.observable( myLocations()[0] );
 
   for(var i=0; i<myLocations().length; i++){
       var marker = new google.maps.Marker({
@@ -46,6 +70,8 @@ var initialize = function(){
 
       attachInfoWindow(marker, i);
   }
+  
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
@@ -59,6 +85,7 @@ function attachInfoWindow(marker, num) {
 
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.open(marker.get('map'), marker);
+
   });
 }
 
@@ -71,4 +98,3 @@ var test = function(){
 >>>>>>> FETCH_HEAD
 }
 
-console.log( myLocations().length );
