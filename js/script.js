a8e88cab5032ad7d0a449a92367d4aa15205378c
Johannes Böhm <polyhickup@gmail.com>
2015-02-15 13:53:15 +0100

    latest updates

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-/* PROJECT GOALS
-
-1.  Write code required to add a full-screen map to your page using the Google Maps API.
-      — DONE
-2.  Write code required to add map markers identifying a number of locations your are interested in within this neighborhood.
-      — DONE
-3.  Implement the search bar functionality to search your map markers.
-      - try it with jQuery autocomplete
-4.  Implement a list view of the identified locations.
-      - 
-5.  Add additional functionality using third-party APIs when a map marker, search result, or list view entry is clicked 
-    (ex. Yelp reviews, Wikipedia, StreetView/Flickr images, etc). If you need a refresher on making AJAX requests to third-party 
-    servers, check out our Intro to AJAX course.
-
-
-
-/* TODOs
-- Model mit Locations definieren
-- View Model aufsetzen
-- html Liste erstellen mit den Locations aus dem Model
-- Locations aus der Liste Klickbar machen udn unterschiedliche Nachricht anzeigen
-- google maps anzeigen
-- location marker aus den model auf der karte anzeigen
-- show window auf pins implementieren
-- show window auf click bei listen elementen implementierne
-- suchergebnisse aus dem textfeld nach markern filtern
-
-*/
-
 // MODEL
 //----------------
 var myLocations = ko.observableArray([
@@ -35,7 +6,10 @@ var myLocations = ko.observableArray([
     street: "Sihlstrasse 24",
     zip: "8001",
     city: "Zürich",
-    latlng: new google.maps.LatLng(47.373399, 8.536846) },
+    latlng: new google.maps.LatLng(47.373399, 8.536846),
+    showItem: function(event, marker){
+      infoWindow.open(map, marker);
+    } },
   { 
     name: "Yooji's",
     street: "Bahnhofstr. 102",
@@ -102,24 +76,6 @@ for (i = 0; i < myLocations().length; i++) {
       }
     });
 
-// Yelp search
-/*
-  var yelpConsumerKey = "-7YvC2J88bRwvoQ9xFPw7Q";
-  var yelpConsumerSecret = "C8lVVq_73NHu5HUyCXWeZ_iZx8Q";
-  var yelpToken = "p-YWs-DhLU3-sWFzKBhTkEpUxfWpy0j3";
-  var yelpSecret = "BgjRpzZ4NJYfmLfTukCwPfvVok0";
-
-  var yelpURL = "http://api.yelp.com/v2/search?term=german+food&location=Hayes&cll=37.77493,-122.419415";
-  console.log(yelpURL);
-  $.ajay({
-    url: yelpURL,
-    dataType: "jsonp",
-    success : function(respone){
-
-    }
-  });
-*/
-
       var infoWindowContent = "<div class='popup'><h1>" + myLocations()[i].name + "</h1><img src='" + streetviewURL + "'><div class='wikiLinks'></div></div>";
       var infoWindow = new google.maps.InfoWindow({
         content: infoWindowContent
@@ -130,30 +86,16 @@ for (i = 0; i < myLocations().length; i++) {
   })(marker, i));
 }
 
-showListElement = function(){
-    console.log("test");
-    infoWindow.open(map, marker);
+showListElement = function(event, marker){
+    console.log(event, marker, map);
+
   }
 
 // VIEW MODEL
 //----------------
 function ViewModel() {
   var self = this;
-
-  //aktuell geklickter Pin
-  //this.currentPOI = ko.observable(0);
   var inputName = ko.observable(input);
-
-
-
-
-
-
-
 }
 
 ko.applyBindings( new ViewModel() );
-
-
-
-
